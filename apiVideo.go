9c813231ee1b4bc27bc9d79fb429519ae719950a
Viktor Oreshkin <imselfish@stek29.rocks>
2018-07-02 13:35:03 +0300

    Add apiVideo.go

---
+++ apiVideo.go
@@ -0,0 +1,70 @@
+package vkCallbackApi
+
+import (
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/mailru/easyjson"
+)
+
+// TODO: Generate Params/Response structs and Methods for all API calls
+
+type VideoGetParams struct {
+	OwnerId  *int
+	Videos   []string
+	AlbumId  *int
+	Count    *int
+	Offset   *int
+	Extended bool
+}
+
+//easyjson:json
+type VideoGetResponse struct {
+	Count int
+	Items []Video
+}
+
+func (vk *VKApi) VideoGet(params VideoGetParams) (*VideoGetResponse, error) {
+	method := "video.get"
+	query := url.Values{}
+
+	if v := params.OwnerId; v != nil {
+		query.Set("owner_id", strconv.Itoa(*v))
+	}
+
+	if v := params.Videos; v != nil {
+		videos := strings.Join(*v, ",")
+		query.Set("videos", videos)
+	}
+
+	if v := params.AlbumId; v != nil {
+		query.Set("album_id", strconv.Itoa(*v))
+	}
+
+	if v := params.Count; v != nil {
+		query.Set("count", strconv.Itoa(*v))
+	}
+
+	if v := params.Offset; v != nil {
+		query.Set("offset", strconv.Itoa(*v))
+	}
+
+	if params.Extended {
+		query.Set("extended", "1")
+	}
+
+	r, err := vk.Request(method, query)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := &VideoGetResponse{}
+	err = easyjson.Unmarshal(r, resp)
+
+	if err != nil {
+		return nil, err
+	} else {
+		return resp, nil
+	}
+}
